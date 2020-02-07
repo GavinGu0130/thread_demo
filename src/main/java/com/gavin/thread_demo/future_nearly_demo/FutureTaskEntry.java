0a8dafbabbf167550089e4f6b91ea9bdc9167c00
Gavin Gu <gavingu0130@hotmail.com>
2020-02-07 12:27:45 +0800

    no message

--- src/main/java/com/gavin/thread_demo/future_nearly_demo/FutureTaskEntry.java
+++ src/main/java/com/gavin/thread_demo/future_nearly_demo/FutureTaskEntry.java
@@ -1,29 +1,43 @@
 package com.gavin.thread_demo.future_nearly_demo;
 
+import java.util.Random;
 import java.util.concurrent.*;
 
 public class FutureTaskEntry {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         // 创建FutureTask
-        FutureTask<Integer> futureTask = new FutureTask<>(()-> {
+        FutureTask<String> futureTask = new FutureTask<>(()-> {
             System.out.println("Entry task");
             ThreadCommon.sleep(3, TimeUnit.SECONDS);
-            return 2+1;
+            return "I'm a return value";
         });
 
         // 创建线程池
         ExecutorService es = Executors.newCachedThreadPool();
         // 提交FutureTask
-        ThreadCommon
         es.submit(futureTask);
 
-        //  当前任务状态
+        //  当前任务完成状态
+        ThreadCommon.sleep(1, TimeUnit.SECONDS);
         System.out.println("The status of task is " + futureTask.isDone() );
-        
-        // 获取计算结果
-        Integer result = futureTask.get();
 
-        System.out.println(result);
+        //  cancel the task 
+        boolean isCancel = false;
+        if((new Random().nextInt(10)  % 2) == 1 ? true : false ) {
+            System.out.println("I'm ready to cancel");
+            futureTask.cancel(true);
+            isCancel = futureTask.isCancelled();
+            System.out.println("The status of task is " + isCancel);
+        }
+
+
+
+        // 获取计算结果
+        if (!isCancel) {
+            String result = futureTask.get();
+
+            System.out.println(result);
+        }
     }
 }
---
+++ src/main/java/com/gavin/thread_demo/lock_demo/ReentrantLockEntry.java
@@ -0,0 +1,7 @@
+package com.gavin.thread_demo.lock_demo;
+
+public class ReentrantLockEntry {
+    public static void main(String[] args) {
+
+    }
+}
