6de67400b077d0832c72c42ef355928b6bc8b0bc
Gavin Gu <gavingu0130@hotmail.com>
2020-06-27 01:11:21 +0800

    * 比较 synchronized & Atomic & LongAdder  * 结论(高并发场景下的性能)：LongAdder > Atomic > Synchronized

---
+++ src/main/java/com/gavin/thread_demo/JustDoIt.java
@@ -0,0 +1,7 @@
+package com.gavin.thread_demo;
+
+public class JustDoIt {
+    public static void main(String[] args) {
+        Object o = new Object();
+    }
+}
---
+++ src/main/java/com/gavin/thread_demo/base/CompareSyncEntry.java
@@ -0,0 +1,79 @@
+package com.gavin.thread_demo.base;
+
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.LongAdder;
+
+/**
+ *
+ * 比较 synchronized & Atomic & LongAdder
+ * 结论(高并发场景下的性能)：LongAdder > Atomic > Synchronized
+ *
+ */
+public class CompareSyncEntry {
+    static long longCount = 0L;
+    final static Object lock = new Object();
+    static AtomicInteger atomicCount = new AtomicInteger(0);
+    static LongAdder longAdderCount = new LongAdder();
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread[] threads = new Thread[1000];
+        for (int i=0; i<threads.length; i++){
+            threads[i] = new Thread(() -> {
+                for (int j=0; j<100000; j++){
+                    atomicCount.incrementAndGet();
+                }
+            });
+        }
+
+        long start = System.currentTimeMillis();
+        for (Thread t : threads){
+            t.start();
+        }
+        for (Thread t : threads){
+            t.join();
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("Atomic: " + atomicCount + "; Time: " + (end-start));
+
+
+        for (int i=0; i<threads.length; i++){
+            threads[i] = new Thread(() -> {
+                for (int j=0; j<100000; j++){
+                    synchronized (lock){
+                        longCount++;
+                    }
+                }
+            });
+        }
+
+        start = System.currentTimeMillis();
+        for (Thread t : threads){
+            t.start();
+        }
+        for (Thread t : threads){
+            t.join();
+        }
+        end = System.currentTimeMillis();
+        System.out.println("Sync: " + longCount + "; Time: " + (end-start));
+
+
+
+        for (int i=0; i<threads.length; i++){
+            threads[i] = new Thread(() -> {
+                for (int j=0; j<100000; j++){
+                    longAdderCount.increment();
+                }
+            });
+        }
+
+        start = System.currentTimeMillis();
+        for (Thread t : threads){
+            t.start();
+        }
+        for (Thread t : threads){
+            t.join();
+        }
+        end = System.currentTimeMillis();
+        System.out.println("LongAdder: " + longAdderCount + "; Time: " + (end-start));
+    }
+}
